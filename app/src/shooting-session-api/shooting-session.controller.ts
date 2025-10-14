85087413f1f93e40b728ea781322ca217a7d902f
Konrad Korus <konrad.korus@flip.shop>
2025-10-14 18:33:09 +0200

    feat: add typeorm package and init api routes

--- app/src/shooting-session-api/shooting-session.controller.ts
+++ app/src/shooting-session-api/shooting-session.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
-@Controller('shooting-session')
+@Controller('shooting-sessions/')
 export class ShootingSessionController {
-  // curl localhost:3000/shooting-session/ping
-  @Get('/ping')
-  public ping(): { status: string } {
-    return { status: 'pong' };
-  }
+  @Get(':id')
+  public getSession(@Param('id') id: string): void {}
+
+  @Get('leaderboard')
+  public getLeaderboard(): void {}
+
+  @Post()
+  public startSession(): void {}
+
+  @Post(':id/events')
+  public addSessionEvent(@Param('id') id: string, @Body() dto: any): void {}
+
+  @Post(':id/finish')
+  public finishSession(@Param('id') id: string): void {}
 }
